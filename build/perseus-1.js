0950ab50d202c0ac6fa7238ff266c2e34d659eb7
Ins Huang <ins.huang@gmail.com>
2017-06-30 20:02:04 +0800

    TEST

--- build/perseus-1.js
+++ build/perseus-1.js
@@ -1,5 +1,6 @@
 /*! Perseus | http://github.com/Khan/perseus */
-// commit 0d077cbf6d38795505ceedff29d0c21c1ea52cdc
+// test
+// commit db1469e6e2fec6876429061930a01bd9e3b6f9b9
 // branch radio-add-image
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Perseus = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
@@ -37882,7 +37883,7 @@ var ExplanationEditor = React.createClass({
                     apiOptions: this.props.apiOptions,
                     content: this.props.explanation,
                     widgets: this.props.widgets,
-                    widgetEnabled: false,
+                    widgetEnabled: true,
                     immutableWidgets: false,
                     onChange: function onChange(props) {
                         var newProps = {};
