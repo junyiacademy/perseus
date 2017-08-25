c2ab5a1ef220bf68c9567aea7eb539dc5a3be9bf
Kevin Lin <kevinptt0323@gmail.com>
2017-08-25 11:40:26 +0800

    make build

--- build/perseus-1.js
+++ build/perseus-1.js
@@ -42073,7 +42073,7 @@ function formatImproper(n, d) {
     if (d === 1) {
         return "" + n;
     } else {
-        return n + "/" + d;
+        return "\\frac{" + n + "}{" + d + "}";
     }
 }
 
@@ -42201,9 +42201,6 @@ var InteractiveNumberLine = React.createClass({
         // Line
 
         graphie.line([range[0] - 25 / scale, 0], [range[1] + 25 / scale, 0], {
-            arrows: "->"
-        });
-        graphie.line([range[1] + 25 / scale, 0], [range[0] - 25 / scale, 0], {
             arrows: "->"
         });
 
@@ -45520,7 +45517,7 @@ function formatImproper(n, d) {
     if (d === 1) {
         return "" + n;
     } else {
-        return n + "/" + d;
+        return "\\frac{" + n + "}{" + d + "}";
     }
 }
 
@@ -45928,9 +45925,7 @@ var NumberLine = React.createClass({
         });
 
         // Draw the number line
-        var center = (range[0] + range[1]) / 2;
-        graphie.line([center, 0], [right, 0], { arrows: "->" });
-        graphie.line([center, 0], [left, 0], { arrows: "->" });
+        graphie.line([left, 0], [right, 0], { arrows: "->" });
     },
 
     setAnswerFromJSON: function setAnswerFromJSON(answerData) {
